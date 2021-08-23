9a4b3466a6ede024dbeb33c33901a641f55e7da4
Anna Khuseinova <annakhuseinova@yandex.ru>
2021-08-23 19:27:45 +0300

    More code

--- src/main/java/com/annakhuseinova/model/EligibilityCheckerApp.java
+++ src/main/java/com/annakhuseinova/model/EligibilityCheckerApp.java
@@ -4,6 +4,7 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
+import javax.jms.MapMessage;
 import javax.jms.Queue;
 import javax.naming.InitialContext;
 
@@ -12,10 +13,14 @@ public class EligibilityCheckerApp {
     public static void main(String[] args) throws Exception{
         InitialContext initialContext = new InitialContext();
         Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+        Queue replyQueue = (Queue) initialContext.lookup("queue/replyQueue");
         try(ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
             JMSContext jmsContext = connectionFactory.createContext()){
             JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
             consumer.setMessageListener(new EligibilityCheckListener());
+            JMSConsumer jmsConsumer = jmsContext.createConsumer(replyQueue);
+            MapMessage message = (MapMessage) jmsConsumer.receive(30000);
+            System.out.println("Patient eligibility is: " + message.getString("eligible"));
         }
     }
 }
